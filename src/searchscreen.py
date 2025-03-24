01650287648257045d996768efc0ebb5a60f77d3
Lifetimecoder <umit@tiric.net>
2025-03-24 13:43:08 -0400

    search screen is updated

--- src/searchscreen.py
+++ src/searchscreen.py
@@ -1,3 +1,4 @@
+from textual import events,binding,on
 from textual.app import App, ComposeResult
 from textual.widgets import Input, ListView, ListItem, Label,Header,Footer
 from textual.containers import Vertical
@@ -12,14 +13,18 @@ class SearchScreen(App):
         layout: vertical;
     }
     Input {
-        width: 50%;
     }
     ListView {
-        width: 50%;
-        height: 30;
+        height: 25;
         overflow: auto;
         padding-left: 1;
     }
+    
+    Vertical {
+        width: 80%;
+        align: center middle;
+        layout: vertical;
+        }
     """
     
     def __init__(self):
@@ -39,7 +44,9 @@ class SearchScreen(App):
     async def on_mount(self) -> None:
         """Runs when the app starts, loads cities from BigQuery."""
         self.source_data = await self.fetch_data()
+        self.query_one(Input).BINDINGS.append(("down", "move foucs to list", "Move focus to list"))
         await self.update_list("")
+        
     
     async def fetch_data(self):
         """Fetch city names from BigQuery and return as a list."""
@@ -59,7 +66,6 @@ class SearchScreen(App):
         """Filters list based on input text."""
         list_view = self.query_one(ListView)
         list_view.clear()
-        
         filtered_data = [datatiem for datatiem in self.source_data if filter_text.lower() in datatiem.lower()]
         for city in filtered_data:
             list_view.append(ListItem(Label(city)))
@@ -75,10 +81,28 @@ class SearchScreen(App):
     
     async def on_list_view_selected(self, event: ListView.Selected):
         """Handles Enter key in list view."""
-        selected_item = event.item.query_one(Label).visual.plain
+        selected_item = event.item.query_one(Label).renderable
         self.exit(selected_item)  # Return selected city
+        
+
+    async def on_key(self, event: events.Key):
+        """Handles down and up key events for swhtching focus between input and listview."""
+        if self.query_one(Input).has_focus:
+            if event.key == "down":
+                listview=self.query_one(ListView)
+                if len(listview.children)>0:
+                    listview.focus()
+                    listview.index=0
+        if self.query_one(ListView).has_focus:
+            if (event.key == "up"  and self.query_one(ListView).index==0):
+                self.query_one(Input).focus()
+                
+
 
 if __name__ == "__main__":
+    
+    
+    print("ffffffffffffffffffffffffffffffffffffffffffffffffffff")
     app = SearchScreen()
     selected_dataitem = app.run()
     print(f"Selected city: {selected_dataitem}")
